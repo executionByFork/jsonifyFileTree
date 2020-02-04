7b453ed630bec88510060bee313588cab3187175
ExecutionByFork <Email Redacted>
2020-02-03 23:35:22 -0600

    add --hash argument

--- jsonifyFileTree.pl
+++ jsonifyFileTree.pl
@@ -1,11 +1,28 @@
 #! /usr/bin/perl
 
 use JSON;
+use Digest::MD5::File qw(file_md5_hex);
 
 my $encoder = JSON->new->ascii->pretty;   # ascii character set, pretty format
 my %tree;                                 # used to build the data structure
 
-my $path=$ARGV[0] || '.';                 # use the command line arg or working dir
+my $path = undef;
+my $hash = 0;
+for (my $i=0; $i < scalar @ARGV; $i++) {
+    if ($ARGV[$i] eq "--hash") {
+        $hash = 1;
+    } else {
+        if (! defined $path) {
+            $path = $ARGV[$i];
+        } else {
+            print "ERROR: Unknown argument: $ARGV[$i]\n";
+            die;
+        }
+    }
+}
+if (! defined $path) {
+    $path = '.';    # use the command line arg or working dir
+}
 
 # print out the JSON encoding of this data structure
 print $encoder->encode(parse_dir("$path"));
@@ -20,13 +37,17 @@ sub parse_dir {
     my @files = grep { ! /^[\.]{1,2}$/ && -f "$path/$_" } readdir($dh);
     closedir($dh);
 
-    my %hash;                                 # set result to an empty hash
-    foreach my $dir (@dirs) {                 # loop the sub directories         
+    my %hash;
+    foreach my $dir (@dirs) {
         my $res = parse_dir("$path/$dir");    # recurse down each path and get results
         $hash{$dir} = $res;
     }
     foreach my $file (@files) {
-        $hash{$file} = undef;
+        $h = undef;
+        if ($hash) {
+            $h = file_md5_hex("$path/$file");
+        }
+        $hash{$file} = $h;
     }
 
     return \%hash;  # return the recursed result
